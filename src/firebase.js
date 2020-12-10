0b5646d742c1d0e0dcaf778c9a06133646364e3d
saifiShaddil <shaddilsaifi021296@gmail.com>
2020-12-10 21:19:24 +0530

    removed database config to dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBJuk6kwZqSw5XMktdO5xsgrZ89SnKgiJ0",
-  authDomain: "todolistredux-53fb5.firebaseapp.com",
-  projectId: "todolistredux-53fb5",
-  storageBucket: "todolistredux-53fb5.appspot.com",
-  messagingSenderId: "978107977224",
-  appId: "1:978107977224:web:b4344d0ae641d85cefcbbd"
+  apiKey: process.env.REACT__APP__apiKey,
+  authDomain: process.env.REACT__APP__authDomain,
+  projectId: process.env.REACT__APP__projectId,
+  storageBucket: process.env.REACT__APP__storageBucket,
+  messagingSenderId: process.env.REACT__APP__messagingSenderId,
+  appId: process.env.REACT__APP__appId,
 });
 
 const auth = firebase.auth();
